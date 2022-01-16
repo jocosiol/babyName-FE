18c56519b99ded617b3a65f5658d1ead3b6fe0d3
jocosiol <jocosiol@gmail.com>
2022-01-16 14:23:49 +0200

    edit

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,9 +2,19 @@ import React from "react";
 
 function Login() {
   return (
-    <div className="min-h-screen bg-yellow-200 py-6 flex flex-col items-center justify-center	">
-      <img src="https://res.cloudinary.com/imgpetapp/image/upload/c_scale,w_148/v1642320920/babyName_logo.png" />
-      <div className="text-blue-500 text-3xl font-bold">Baby Name</div>
+    <div className="min-h-screen bg-yellow-200 py-6 flex flex-col justify-center">
+      <div className="flex flex-col items-center">
+        <img src="https://res.cloudinary.com/imgpetapp/image/upload/c_scale,w_148/v1642320920/babyName_logo.png" />
+        <div className="text-teal-600 text-3xl font-bold font-Montserrat">
+          Baby Name
+        </div>
+      </div>
+      <form>
+          <div>
+              <lable>Email</lable>
+              <input type="text"></input>
+          </div>
+      </form>
     </div>
   );
 }
