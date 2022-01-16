decc73a190b799b6ffc1e89a25e01df2fa06abe7
jocosiol <jocosiol@gmail.com>
2022-01-16 10:00:50 +0200

    add structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import AppContext from "./context/AppContext";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello World!
-    </h1>
+    <AppContext.Provider value={{}}>
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+        </Routes>
+      </Router>
+    </AppContext.Provider>
   );
 }
 
