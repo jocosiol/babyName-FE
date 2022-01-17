5c69ab209d618276e79441c4893cc85823d7a54f
MatiasOgorinsky <matiogorinsky@gmail.com>
2022-01-17 11:45:59 +0200

    favorites page

---
+++ src/pages/Favorites.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import TopBar from "../components/TopBar";
+
+function Favorites() {
+  return (
+    <>
+      <div className="min-h-screen bg-yellow-200 py-6 flex flex-col font-Montserrat">
+        <TopBar name="Favorites" />
+        <div className="flex flex-col justify-center items-center mt-5">
+          <label className="flex  ">There is a match!</label>
+          <button class="bg-pink-500 hover:bg-pink-700 text-white font-bold py-3 px-24 w-1/4 rounded">Marcela</button>
+        </div>
+        <div className="flex flex-col justify-center items-center mt-5">
+          <label className="flex  ">My Favorites names</label>
+          <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-3 px-24 w-1/4 mb-1 rounded">Jessica</button>
+          <button class="bg-pink-500 hover:bg-pink-700 text-white font-bold py-3 px-24 w-1/4 mb-1 rounded">Marcela</button>
+          <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-3 px-24 w-1/4 mb-1 rounded">Moshe</button>
+          <button class="bg-pink-500 hover:bg-pink-700 text-white font-bold py-3 px-24 w-1/4  mb-1 rounded">Marcela</button>
+          <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-3 px-24 w-1/4 mb-1 rounded">Matias</button>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Favorites;
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -2,8 +2,7 @@ import React, { useState } from "react";
 import LeftArrow from "../icons/LeftArrow";
 
 function SignUp() {
-  const [connectToPartnerSwitcher, setConnectToPartnerSwitcher] =
-    useState(false);
+  const [connectToPartnerSwitcher, setConnectToPartnerSwitcher] = useState(false);
 
   const handleSwitcherChange = () => {
     setConnectToPartnerSwitcher((prev) => {
@@ -13,7 +12,9 @@ function SignUp() {
 
   return (
     <div className="min-h-screen bg-yellow-200 py-6 flex flex-col justify-center font-Montserrat">
-      <div className="w-5 h-5 mx-10 mt-1 "><LeftArrow /></div>
+      <div className="w-5 h-5 mx-10 mt-1 ">
+        <LeftArrow />
+      </div>
       <div className="flex grow flex-col justify-center items-center mb-10">
         <img src="https://res.cloudinary.com/imgpetapp/image/upload/c_scale,w_148/v1642320920/babyName_logo.png" />
         <div className="text-teal-600 text-3xl font-bold">Baby Name</div>
@@ -21,41 +22,23 @@ function SignUp() {
       <form className="flex flex-col justify-center">
         <div className="flex flex-col border-b border-black mx-10 mt-5">
           <label className="opacity-50">Name</label>
-          <input
-            className="appearance-none bg-transparent border-none"
-            type="text"
-          ></input>
+          <input className="appearance-none bg-transparent border-none" type="text"></input>
         </div>
         <div className="flex flex-col border-b border-black mx-10 mt-5">
           <label className="opacity-50">Email</label>
-          <input
-            className="appearance-none bg-transparent border-none"
-            type="password"
-          ></input>
+          <input className="appearance-none bg-transparent border-none" type="password"></input>
         </div>
         <div className="flex flex-col border-b border-black mx-10 mt-5">
           <label className="opacity-50">Password</label>
-          <input
-            className="appearance-none bg-transparent border-none"
-            type="password"
-          ></input>
+          <input className="appearance-none bg-transparent border-none" type="password"></input>
         </div>
         <div className="flex flex-col border-b border-black mx-10 mt-5">
           <label className="opacity-50">Retype Password</label>
-          <input
-            className="appearance-none bg-transparent border-none"
-            type="password"
-          ></input>
+          <input className="appearance-none bg-transparent border-none" type="password"></input>
         </div>
         <div className="flex flex-row mx-10 mt-5">
           <div className="form-check form-switch">
-            <input
-              className="form-check-input appearance-none w-9 -ml-10 rounded-full float-left h-5 align-top bg-no-repeat bg-contain bg-gray-300 focus:outline-none cursor-pointer shadow-sm"
-              type="checkbox"
-              role="switch"
-              id="flexSwitchCheckDefault56"
-              onChange={handleSwitcherChange}
-            />
+            <input className="form-check-input appearance-none w-9 -ml-10 rounded-full float-left h-5 align-top bg-no-repeat bg-contain bg-gray-300 focus:outline-none cursor-pointer shadow-sm" type="checkbox" role="switch" id="flexSwitchCheckDefault56" onChange={handleSwitcherChange} />
           </div>
           <label className="opacity-50">Connect to partner</label>
         </div>
@@ -63,17 +46,12 @@ function SignUp() {
         {connectToPartnerSwitcher && (
           <div className="flex flex-col border-b border-black mx-10 mt-5">
             <label className="opacity-50">Partner key</label>
-            <input
-              className="appearance-none bg-transparent border-none"
-              type="password"
-            ></input>
+            <input className="appearance-none bg-transparent border-none" type="password"></input>
           </div>
         )}
 
         <div className="flex justify-center mt-20">
-          <button className="bg-emerald-400 hover:bg-emerald-300 cursor-pointer py-3 px-20 rounded-full text-slate-800 font-semibold">
-            Sign up
-          </button>
+          <button className="bg-emerald-400 hover:bg-emerald-300 cursor-pointer py-3 px-20 rounded-full text-slate-800 font-semibold">Sign up</button>
         </div>
       </form>
     </div>
