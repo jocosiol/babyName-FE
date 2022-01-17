2cca3f01a6a79984a3081cd5d4a7b581e7d4613d
jocosiol <jocosiol@gmail.com>
2022-01-17 17:58:50 +0200

    Name Page

--- src/components/NameDetailCard.jsx
+++ src/components/NameDetailCard.jsx
@@ -2,24 +2,23 @@ import React from "react";
 
 function NameDetailCard(props) {
   return (
-    <div class="flex justify-center">
-      <div class="block p-6 rounded-lg shadow-lg bg-white max-w-sm  bg-[#99C8EC] border border-black">
-        <div class="flex flex-col items-center">
-          <h3 class="text-gray-900 text-xl leading-tight font-medium mb-2">{props.name}</h3>
-          <p class="text-gray-900 text-base leading-tight font-medium mb-4">{props.gender}</p>
+
+      <div className="flex flex-col grow p-6 rounded-lg bg-[#99C8EC] border border-black justify-center font-Montserrat mt-5">
+        <div className="flex flex-col items-center">
+          <h3 className="text-gray-900 text-2xl leading-tight mb-2 font-semibold">{props.name}</h3>
+          <p className="text-gray-900 text-base leading-tight font-medium mb-4">{props.gender}</p>
         </div>
 
-        <div class="flex flex-col items-center">
-          <h3 class="text-gray-900 text-xl leading-tight font-medium mb-2  m-4">Meaning:</h3>
-          <p class="text-gray-700 text-base mb-4">{props.meaning}</p>
+        <div className="flex flex-col items-center">
+          <h3 className="text-gray-900 text-base leading-tight font-medium mb-2  m-4">Meaning:</h3>
+          <p className="text-center text-gray-700 text-base mb-4">{props.meaning}</p>
         </div>
 
-        <div class="flex flex-col items-center">
-          <h3 class="text-gray-900 text-xl leading-tight font-medium mb-2 flex flex-col items-center m-4">Category:</h3>
-          <p class="text-gray-700 text-base mb-4">{props.category}</p>
-        </div>
+        {/* <div className="flex flex-col items-center">
+          <h3 className="text-gray-900 text-xl leading-tight font-medium mb-2 flex flex-col items-center m-4">Category:</h3>
+          <p className="text-gray-700 text-base mb-4">{props.category}</p>
+        </div> */}
       </div>
-    </div>
   );
 }
 
