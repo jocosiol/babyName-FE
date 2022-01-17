cf4bf3a1783caf6250c578ee43999518cdf13ea9
MatiasOgorinsky <matiogorinsky@gmail.com>
2022-01-17 14:43:28 +0200

    styling

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Card(props) {
   return (
     <div class="flex justify-center">
-      <div class="block p-6 rounded-lg shadow-lg bg-white max-w-sm  bg-[#99C8EC]">
+      <div class="block p-6 rounded-lg shadow-lg bg-white max-w-sm  bg-[#99C8EC] border border-black">
         <div class="flex flex-col items-center">
           <h3 class="text-gray-900 text-xl leading-tight font-medium mb-2">{props.name}</h3>
           <p class="text-gray-900 text-base leading-tight font-medium mb-4">{props.gender}</p>
