c751d639b4fa9a6d203056bc748605253fb585ff
jocosiol <jocosiol@gmail.com>
2022-01-18 17:29:08 +0200

    add fav from set

--- src/components/NameCard.jsx
+++ src/components/NameCard.jsx
@@ -6,17 +6,30 @@ function NameCard(props) {
   const { setSelectedName, setSelectedKey } = useContext(AppContext);
 
   const handleNameCardClick = () => {
-    //console.log(props.name);
     setSelectedName(props.name);
     setSelectedKey(props.index);
   };
 
   return (
-    <Link to={{ pathname: `/name/${props.index}` }}>
-      <div className="flex flex-col items-center font-semibold bg-emerald-300 border border-black mt-5 py-5 rounded-xl cursor-pointer" onClick={handleNameCardClick}>
-        {props.name}
-      </div>
-    </Link>
+    <>
+      {!props.favorited ? (
+        <Link to={{ pathname: `/name/${props.index}` }}>
+          <div
+            className="flex flex-col items-center font-semibold bg-indigo-400 border border-black mt-5 py-5 rounded-xl cursor-pointer"
+            onClick={handleNameCardClick}
+          >
+            {props.name}
+          </div>
+        </Link>
+      ) : (
+        <div
+          className="flex flex-col items-center font-semibold bg-indigo-400 border border-black mt-5 py-5 rounded-xl"
+          onClick={handleNameCardClick}
+        >
+          {props.name}
+        </div>
+      )}
+    </>
   );
 }
 
