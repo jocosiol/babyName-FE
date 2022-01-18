fd46699b838c319e5761e2b0e644f9af81acedc7
jocosiol <jocosiol@gmail.com>
2022-01-18 16:24:57 +0200

    change select to buttons

--- src/components/NamePreference.jsx
+++ src/components/NamePreference.jsx
@@ -5,31 +5,25 @@ function NamePreference() {
   const { setGender } = useContext(AppContext);
 
   return (
-    <div className="flex flex-col bg-cyan-300  justify-enter items-center border border-black rounded-lg my-5 py-3 font-Montserrat">
-      <div className="w-64">
-        <select
-          className="appearance-none w-full bg-white border border-black px-4 py-2 pr-8 rounded leading-tight focus:outline-none focus:shadow-outline"
-          defaultValue="title"
-          onChange={(e) => {
-            setGender(e.target.value);
+    <div className="flex flex-row bg-cyan-300 justify-enter items-center space-x-3 border border-black rounded-lg my-5 p-3 font-Montserrat">
+        <div
+          id="male"
+          className="grow flex justify-center bg-blue-400 hover:bg-blue-500 border border-black rounded-lg p-5 font-semibold cursor-pointer"
+          onClick={(e) => {
+            setGender(e.target.id);
           }}
         >
-          <option value="title">Gender</option>
-          <option value="male">Male</option>
-          <option value="female">Female</option>
-        </select>
-      </div>
-
-      {/* <div className="w-64 mt-3">
-        <select
-          className="appearance-none w-full bg-white border border-black px-4 py-2 pr-8 rounded leading-tight focus:outline-none focus:shadow-outline"
-          defaultValue="title"
+          Male
+        </div>
+        <div
+          id="female"
+          className="grow flex justify-center bg-pink-400 hover:bg-pink-500 border border-black rounded-lg p-5 font-semibold cursor-pointer"
+          onClick={(e) => {
+            setGender(e.target.id);
+          }}
         >
-          <option value="title">Category</option>
-          <option value="english">English</option>
-          <option value="asian">Asian</option>
-        </select>
-      </div> */}
+          Female
+        </div>
     </div>
   );
 }
