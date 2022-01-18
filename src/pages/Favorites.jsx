247722a180b44ab166d9f4aa7af99aa08a8448b4
MatiasOgorinsky <matiogorinsky@gmail.com>
2022-01-18 09:46:20 +0200

    change on favorites

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import TopBar from "../components/TopBar";
+import NameCard from "../components/NameCard";
 
 function Favorites() {
   return (
@@ -8,8 +9,9 @@ function Favorites() {
         <TopBar name="Favorites" />
         <div className="flex flex-col justify-center items-center mt-5">
           <label className="flex  ">There is a match!</label>
-          <button class="bg-pink-500 hover:bg-pink-700 text-white font-bold py-3 px-24 w-1/4 rounded">Marcela</button>
+          <NameCard></NameCard>
         </div>
+
         <div className="flex flex-col justify-center items-center mt-5">
           <label className="flex  ">My Favorites names</label>
           <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-3 px-24 w-1/4 mb-1 rounded">Jessica</button>
